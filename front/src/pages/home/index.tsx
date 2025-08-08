b1eadd3b7d6119f153fd67566af4296a85be3bc7
Pedr0LA <pedroluizabreu06@gmail.com>
2025-08-08 14:01:10 -0300

    feature: adição de alguns elementos responsivos na home

--- front/src/pages/home/index.tsx
+++ front/src/pages/home/index.tsx
@@ -11,47 +11,51 @@ export default function Home(){
         <div>
             <Header/>
 
-            <div className="flex justify-center items-center w-full bg-[#F8F8F8]">
+            <div className="flex h-100 lg:h-auto justify-center items-center w-full bg-[#F8F8F8]">
                 <div className="w-1/3">
                     <img src={noImage} alt="" className="w-full h-auto opacity-5"/>
                 </div>
 
-                <div className="flex absolute flex-col justify-center w-[672px] gap-6 items-center z-10">
-                    <label className="font-bold text-7xl">Style Redefined</label>
+                <div className="flex absolute flex-col justify-center w-full  md:max-xl:w-1/2 xl:w-1/3 gap-6 items-center z-10">
+                    <label className="font-bold text-center text-5xl lg:text-7xl">Style Redefined</label>
 
-                    <label className="flex justify-center text-2xl text-[#6B7280]">Discover the latest trends in fashion. Premium quality, sustainable materials, timeless designs.</label>
+                    <label className="text-xl text-center lg:text-2xl text-[#6B7280]">Discover the latest trends in fashion. Premium quality, sustainable materials, timeless designs.</label>
 
-                    <div className="flex gap-4">
-                        <button className="flex justify-between px-8 items-center w-[182px] h-[48px] text-lg text-[#F9FAFB] rounded-lg cursor-pointer bg-[#030711]">
+                    <div className="flex flex-col w-full justify-center items-center gap-4 lg:flex-row">
+                        <button className="flex justify-center gap-4 px-8 items-center w-9/12 md:w-[182px] h-[48px] text-lg text-[#F9FAFB] rounded-lg cursor-pointer bg-[#030711]">
                             Shop Now
                             <img src={arrow} alt=""/>
                         </button>
 
-                        <button className="flex justify-center items-center w-[182px] h-[48px] text-lg text-[#030711] rounded-lg border-[#E5E7EB] border-1 cursor-pointer bg-[#FFFFFF]">
+                        <button className="flex justify-center items-center w-9/12 md:w-[182px] h-[48px] text-lg text-[#030711] rounded-lg border-[#E5E7EB] border-1 cursor-pointer bg-[#FFFFFF]">
                             View Collection
                         </button>
                     </div>
                 </div>
             </div>
 
-            <div className="flex flex-wrap justify-around w-full py-16">
-                <InfoIcons       
-                    image = {shippingIcon}
-                    title = "Free Shipping"
-                    subtitle = "Free shipping on orders over $100"
-                />
+            <div className="flex flex-col gap-20 px-10">
+                <div className="flex flex-wrap justify-around w-full py-16">
+                    <InfoIcons       
+                        image = {shippingIcon}
+                        title = "Free Shipping"
+                        subtitle = "Free shipping on orders over $100"
+                    />
 
-                <InfoIcons       
-                    image = {returnsIcon}
-                    title = "Easy Returns"
-                    subtitle = "30-day hassle-free returns"
-                />
+                    <InfoIcons       
+                        image = {returnsIcon}
+                        title = "Easy Returns"
+                        subtitle = "30-day hassle-free returns"
+                    />
 
-                <InfoIcons       
-                    image = {securityIcon}
-                    title = "Secure Payment"
-                    subtitle = "Your payment information is safe"
-                />
+                    <InfoIcons       
+                        image = {securityIcon}
+                        title = "Secure Payment"
+                        subtitle = "Your payment information is safe"
+                    />
+                </div>
+
+
             </div>
         </div>
     )
