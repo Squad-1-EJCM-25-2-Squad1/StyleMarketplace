e2cf08e70432eb7d25653dbc6faaea645480ef71
Pedr0LA <pedroluizabreu06@gmail.com>
2025-08-09 20:16:24 -0300

    feature: mudança da fonte para sistemas além do windows

--- front/tailwind.config.ts
+++ front/tailwind.config.ts
@@ -7,5 +7,13 @@ export default {
         "./src/styles/*.{js,jsx,ts,tsx}",
         "./src/app/**/*.{js,jsx,ts,tsx}",
         "./src/pages/**/*.{js,jsx,ts,tsx}"
-    ]
+    ],
+    theme: {
+        extend: {
+            //Em teoria isso faz com que fique padrão a Segoe UI para todos os sistemas
+            fontFamily: {
+                'sans': ['"Segoe UI"', 'Roboto', 'system-ui', 'sans-serif'],
+            }
+        },
+    },
 } satisfies Config;
