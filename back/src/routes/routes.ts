1c977e010e0c053962122f34b99269fd0e2731b3
Ulvv <guilhermebritodemorais06@gmail.com>
2025-08-10 20:00:16 -0300

    feature: Routes.ts- added update route

--- back/src/routes/routes.ts
+++ back/src/routes/routes.ts
@@ -6,6 +6,7 @@ const router = Router()
 router.post('/signup', UserController.signup);
 router.post('/login', UserController.login);
 router.get('/user/:userId', UserController.readUser);
+router.get('/user/:userId', UserController.updateUser);
 router.delete('/user/:userId', UserController.deleteUser);
 
 //rota de teste para pegar os IDs, pode ser apagada depois
