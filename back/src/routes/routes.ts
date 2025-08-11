f7630e91539d37689667bc35878a5f24e963bd28
Isaac Vianna <isaac.maiolino@gmail.com>
2025-08-11 12:33:53 -0300

    feature: implementação de routes.ts e alteração de server.ts para realizar o import e use de routes.

---
+++ back/src/routes/routes.ts
@@ -0,0 +1,29 @@
+import { Router } from 'express';
+import ColorController from '../controllers/ColorController';
+import SizeController from '../controllers/SizeController';
+import OfferController from '../controllers/OfferController';
+
+const router = Router();
+
+// --- Rotas de Cores ---
+router.post('/color', ColorController.createColor);
+router.get('/color/:id', ColorController.readColor);
+router.get('/color', ColorController.readAllColors); 
+router.put('/color/:id', ColorController.updateColor);
+router.delete('/color/:id', ColorController.deleteColor);
+
+// --- Rotas de Tamanhos ---
+router.post('/size', SizeController.createSize);
+router.get('/size/:id', SizeController.readSize);
+router.get('/size', SizeController.readAllSizes); 
+router.put('/size/:id', SizeController.updateSize);
+router.delete('/size/:id', SizeController.deleteSize);
+
+// --- Rotas de Ofertas ---
+router.post('/offer', OfferController.createOffer);
+router.get('/offer/:id', OfferController.readOffer);
+router.get('/offer', OfferController.readAllOffers); 
+router.put('/offer/:id', OfferController.updateOffer);
+router.delete('/offer/:id', OfferController.deleteOffer);
+
+export default router; 
