9a6dc0f814fa36fa10dc49d4b989b91c870f50eb
luiza <castro.b.luiza@gmail.com>
2025-08-11 20:58:11 -0300

    fix: minor changes to fix the merge

--- back/src/controllers/UserController.ts
+++ back/src/controllers/UserController.ts
@@ -2,160 +2,165 @@ import { Prisma, PrismaClient } from "@prisma/client";
 import { Request, Response } from "express";
 import auth from "../config/auth";
 
-
 const prisma = new PrismaClient();
 
 export class UserController {
-	public static async signup(request: Request, response: Response) {
-		try {
-			const { firstName, lastName, imageSrc, gender, email, birthDate, phone, password } = request.body;
-			const { hash, salt } = auth.generatePassword(password);
+  public static async signup(request: Request, response: Response) {
+    try {
+      const {
+        firstName,
+        lastName,
+        imageSrc,
+        gender,
+        email,
+        birthDate,
+        phone,
+        password,
+      } = request.body;
+      const { hash, salt } = auth.generatePassword(password);
 
-			// Cria o carrinho vazio
-			const cart = await prisma.cart.create({
-				data: {}
-			});
+      // Cria o carrinho vazio
+      const cart = await prisma.cart.create({
+        data: {},
+      });
 
-			// Cria a wishlist vazia
-			const wishlist = await prisma.wishlist.create({
-				data: {}
-			});
+      // Cria a wishlist vazia
+      const wishlist = await prisma.wishlist.create({
+        data: {},
+      });
 
-			const createInput: Prisma.UserCreateInput = {
-				firstName: firstName,
-				lastName: lastName,
-				email: email,
-                birthDate: birthDate,
-                phone: phone,
-				imageSrc: imageSrc,
-				gender: gender,
-				cart: {
- 					 connect: { id: cart.id } 
-				},
-				wishlist:{
-					connect: {id: wishlist.id}
-				},
-				hash: hash,
-				salt: salt
-			};
+      const createInput: Prisma.UserCreateInput = {
+        firstName: firstName,
+        lastName: lastName,
+        email: email,
+        birthDate: birthDate,
+        phone: phone,
+        imageSrc: imageSrc,
+        gender: gender,
+        cart: {
+          connect: { id: cart.id },
+        },
+        wishlist: {
+          connect: { id: wishlist.id },
+        },
+        hash: hash,
+        salt: salt,
+      };
 
-			const createdUser = await prisma.user.create({
-				data: createInput,
-			});
+      const createdUser = await prisma.user.create({
+        data: createInput,
+      });
 
-			if(!createdUser){
-				return response.status(404).json();
-			}
+      if (!createdUser) {
+        return response.status(404).json();
+      }
 
-			response.status(201).json(createdUser);
-			
-		} catch (error: any) {
-			response.status(500).json({ message: error.message });
-		}
-	}
-
-	static async login(request: Request, response: Response) {
-
-
-        try {
-            
-            const {email, password} = request.body;
-    
-            const user = await prisma.user.findUnique({
-                where:{email: email}
-            });
-
-            if(!user)
-                return response.status(400).json({message:"usuario não existente"})
-
-            const {hash, salt} = user
-
-            if(!auth.checkPassword(password, hash, salt)){
-                return response.status(400).json({message:"Senha incorreta"})
-            }
-
-			return response.status(201).json({message:"Login completo"})
-
-        } catch (error) {
-            return response.status(500).json()
-
-        }   
+      response.status(201).json(createdUser);
+    } catch (error: any) {
+      response.status(500).json({ message: error.message });
     }
+  }
 
-	public static async readUser(request: Request, response: Response){
-		try {
-			const { userId } = request.params;
-			const foundUser = await prisma.user.findUnique({
-				where: {
-					id: userId,
-				},
-				include: {
-					wishlist: true,
-					cart: true,
-					orders: true,
-					reviews: true,
-				}
-			});
-			response.status(201).json(foundUser);
+  static async login(request: Request, response: Response) {
+    try {
+      const { email, password } = request.body;
 
-		} catch (error: any) {
-			response.status(500).json({ message: error.message});
+      const user = await prisma.user.findUnique({
+        where: { email: email },
+      });
 
-		}
-	}
+      if (!user)
+        return response.status(400).json({ message: "usuario não existente" });
 
-	public static async deleteUser(request: Request, response: Response) {
-		try {
-			const { userId } = request.params;
+      const { hash, salt } = user;
 
-			const deletedUser = await prisma.user.delete({
-				where: {
-					id: userId,
-				},
-			});
-			response.status(200).json(deletedUser);
-		} catch (error: any) {
-			response.status(500).json({ message: error.message });
-		}
-	}
+      if (!auth.checkPassword(password, hash, salt)) {
+        return response.status(400).json({ message: "Senha incorreta" });
+      }
 
-	public static async updateUser(request: Request, response: Response){
-		try {
-			const { userId } = request.params;
-			const { firstName, lastName, imageSrc, gender, email, birthDate, phone} = request.body;
+      return response.status(201).json({ message: "Login completo" });
+    } catch (error) {
+      return response.status(500).json();
+    }
+  }
 
-			const createInput: Prisma.UserUpdateInput = {
-				firstName: firstName,
-				lastName: lastName,
-				email: email,
-                birthDate: birthDate,
-                phone: phone,
-				imageSrc: imageSrc,
-				gender: gender
-			};
+  public static async readUser(request: Request, response: Response) {
+    try {
+      //   const { id } = request.user as string;
+      const { id } = request.params;
 
-			const updatedUser = await prisma.user.update({
-				data: createInput,
-				where: {
-					id: userId,
-				}
-			});
+      try {
+        const foundUser = await prisma.user.findUnique({
+          where: {
+            id: id,
+          },
+          include: {
+            wishlist: true,
+            cart: true,
+            orders: true,
+            reviews: true,
+          },
+        });
+        return response.status(201).json(foundUser);
+      } catch (err) {
+        return response.status(400).json("Usuário não encontrado");
+      }
+    } catch (error: any) {
+      return response.status(500).json({ message: error.message });
+    }
+  }
 
-			response.status(200).json(updatedUser);
+  public static async deleteUser(request: Request, response: Response) {
+    try {
+      const { userId } = request.params;
 
-		} catch (error: any) {
-			response.status(500).json({ message: error.message });
-		}
-	}
+      const deletedUser = await prisma.user.delete({
+        where: {
+          id: userId,
+        },
+      });
+      response.status(200).json(deletedUser);
+    } catch (error: any) {
+      response.status(500).json({ message: error.message });
+    }
+  }
 
+  public static async updateUser(request: Request, response: Response) {
+    try {
+      const { userId } = request.params;
+      const { firstName, lastName, imageSrc, gender, email, birthDate, phone } =
+        request.body;
 
-	public static async readAllUsers(request: Request, response: Response) {
-		try {
-			const users = await prisma.user.findMany({});
+      const createInput: Prisma.UserUpdateInput = {
+        firstName: firstName,
+        lastName: lastName,
+        email: email,
+        birthDate: birthDate,
+        phone: phone,
+        imageSrc: imageSrc,
+        gender: gender,
+      };
 
-			response.status(200).json(users);
-		} catch (error: any) {
-			response.status(500).json({ message: error.message });
-		}
-	}
+      const updatedUser = await prisma.user.update({
+        data: createInput,
+        where: {
+          id: userId,
+        },
+      });
+
+      response.status(200).json(updatedUser);
+    } catch (error: any) {
+      response.status(500).json({ message: error.message });
+    }
+  }
+
+  public static async readAllUsers(request: Request, response: Response) {
+    try {
+      const users = await prisma.user.findMany({});
+
+      response.status(200).json(users);
+    } catch (error: any) {
+      response.status(500).json({ message: error.message });
+    }
+  }
 }
--- back/src/models/migrations/20250809205735_init/migration.sql
+++
@@ -1,5 +0,0 @@
--- DropForeignKey
-ALTER TABLE "public"."User" DROP CONSTRAINT "User_wishlistId_fkey";
-
--- AddForeignKey
-ALTER TABLE "public"."User" ADD CONSTRAINT "User_wishlistId_fkey" FOREIGN KEY ("wishlistId") REFERENCES "public"."Wishlist"("id") ON DELETE CASCADE ON UPDATE CASCADE;
