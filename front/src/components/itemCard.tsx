d3a1087414eaeccb2c8770b6092c4794737a002b
Pedr0LA <pedroluizabreu06@gmail.com>
2025-08-13 20:18:41 -0300

    feature: criado o card dos items em preparação para o import da biblioteca de carrossel

---
+++ front/src/components/itemCard.tsx
@@ -0,0 +1,30 @@
+import { useState } from "react";
+import wishlist from "../assets/home/favorito.svg";
+import wishlisted from "../assets/sales/wishlisted.svg";
+
+interface ItemCardProps{
+    image: string,
+    discount: number,
+}
+
+export default function ItemCard(props: ItemCardProps){
+
+    const [wishlistedItem, setWishlistedItem] = useState<boolean>(false)
+
+    return(
+        <img src={props.image} alt="" className="relative h-85 w-85">
+            <span className="absolute text-white text-xs font-semibold py-1 px-3 rounded-full bg-red-600 top-4 left-4">-{props.discount}%</span>
+
+            <button onClick={() => setWishlistedItem(!wishlistedItem)} className="absolute top-4 right-4 bg-white rounded-xl p-4
+            transition duration-300 ease-in-out filter brightness-100 hover:brightness-90">
+                {!wishlistedItem && (
+                    <img src={wishlist} alt=""/>
+                )}
+
+                {wishlistedItem && (
+                    <img src={wishlisted} alt=""/>
+                )}
+            </button>
+        </img>
+    )
+}
